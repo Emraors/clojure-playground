b16a5e8a353229765ee3e13a35f7daec8dd8d1ce
Emraors <eros.martinelli@hotmail.it>
2024-01-16 19:19:11 +0100

    Finish day 10

--- advent-of-code-2021/src/advent_of_code_2021/day10.clj
+++ advent-of-code-2021/src/advent_of_code_2021/day10.clj
@@ -11,13 +11,19 @@
 (defn closing? [ch] (contains? #{\} \> \] \)} ch))
 
 (def initial-state
-  {:stack '(), :correct? true, :complete? true, :first-incorrect nil})
+  {:stack '(),
+   :correct? true,
+   :complete? true,
+   :first-incorrect nil,
+   :to-complete '()})
 
 (defn complete? [state] (:complete state))
 
-(defn corrupted? [state] (:correct? state))
+(defn correct? [state] (:correct? state))
 
 (defn first-incorrect [state] (:first-incorrect state))
+
+(defn to-complete [state] (:to-complete state))
 
 (defn dispatch-fn
   [_ ch]
@@ -28,37 +34,41 @@
 (defmulti update-state dispatch-fn)
 
 (defmethod update-state :opening
-  [{:keys [stack], :as state} ch]
-  (-> state
-      (assoc :stack (cons ch stack))
-      (assoc :complete? false)))
+  [{:keys [stack to-complete], :as state} ch]
+  (let [open-closed {\[ \], \{ \}, \< \>, \( \)}]
+    (-> state
+        (assoc :stack (cons ch stack)
+               :to-complete (cons (open-closed ch) to-complete)
+               :complete? false))))
 
 (defmethod update-state :closing
-  [{:keys [stack], :as state} ch]
-  (let [open-closed {\[ \], \{ \}, \< \>, \( \)}]
-    (cond
-      (empty? stack) (-> state
-                         (assoc :stack '())
-                         (assoc :complete? false)
-                         (assoc :correct? false)
-                         (assoc :first-incorrect ch))
-      (= (open-closed (first stack)) ch)
-        (-> state
-            (assoc :stack (rest stack))
-            (assoc :correct? true)
-            (update :complete? (fn [_] (empty? (rest stack)))))
-      :else (-> state
-                (assoc :stack stack)
-                (assoc :complete? false)
-                (assoc :correct? false)
-                (assoc :first-incorrect ch)))))
+  [{:keys [stack to-complete], :as state} ch]
+  (cond
+    (empty? stack) (-> state
+                       (assoc :stack '()
+                              :to-complete '()
+                              :complete? false
+                              :correct? false
+                              :first-incorrect ch))
+    (= (first to-complete) ch) (-> state
+                                   (assoc :stack (rest stack)
+                                          :to-complete (rest to-complete)
+                                          :correct? true)
+                                   (update :complete?
+                                           (fn [_] (empty? (rest stack)))))
+    :else (-> state
+              (assoc :stack stack
+                     :to-complete to-complete
+                     :complete? false
+                     :correct? false
+                     :first-incorrect ch))))
 
 (defmethod update-state :default [_ _] nil)
 
 (defn evaluate-line
   [line]
   (reduce (fn [state ch]
-            (if (corrupted? state) (update-state state ch) (reduced state)))
+            (if (correct? state) (update-state state ch) (reduced state)))
     initial-state
     line))
 
@@ -90,3 +100,44 @@
 (assert (= 26397 (get-first-solution (parse-input example-input))))
 
 (assert (= 318099 (get-first-solution (parse-input input))))
+
+
+(assert (= (-> "[({(<(())[]>[[{[]{<()<>>"
+               (evaluate-line)
+               (to-complete))
+           '(\} \} \] \] \) \} \) \])))
+
+(assert (= (-> "[(()[<>])]({[<{<<[]>>("
+               (evaluate-line)
+               (to-complete))
+           '(\) \} \> \] \} \))))
+
+(defn autocomplete-score
+  [line]
+  (let [values {\) 1, \] 2, \} 3, \> 4}]
+    (reduce (fn [partial-score value] (+ (* 5 partial-score) (values value)))
+      0
+      line)))
+
+(defn middle-value
+  [numbers]
+  (let [sorted (sort numbers)
+        n (count sorted)
+        mid-index (quot n 2)]
+    (if (odd? n)
+      (nth sorted mid-index)
+      (/ (+ (nth sorted (dec mid-index)) (nth sorted mid-index)) 2))))
+
+(defn get-second-solution
+  [input]
+  (->> input
+       (map evaluate-line)
+       (filter correct?)
+       (filter (complement complete?))
+       (map to-complete)
+       (map autocomplete-score)
+       (middle-value)))
+
+(assert (= 288957 (get-second-solution (parse-input example-input))))
+
+(assert (= 2389738699 (get-second-solution (parse-input input))))
